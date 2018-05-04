b7cdce91b1d5dbccbee0b31ac09ecc245ca12107
Nedu Robert <nedumrobert@gmail.com>
2018-05-04 18:55:25 +0100

    showing list of projects

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentDidMount() {
+    this.getProjects();
+  }
+
+  getProjects = () => {
+    axios.get('http://localhost:5000/api/projects/')
+    .then(res => {
+      this.setState({ projects: res.data});
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.projects.map(project => {
+          return <div key={project.id}>Project Name: {project.name}</div>
+        })}
       </div>
     );
   }
